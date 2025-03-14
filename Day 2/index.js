d5eaaf43e4a31c3746f50c45e01752ca0a6a1a6c
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2025-03-14 09:38:59 +0500

    Day - 2 Challenge Completed

---
+++ Day 2/index.js
@@ -0,0 +1,43 @@
+
+// alert("Hello Friends")
+// alert("learn HTML Course")
+// alert("learn CSS Course")
+// alert("learn JAVASCRIPT Course")
+// alert("learn REACTJS Course")
+// alert("learn ANGULARJS Course")
+// alert("learn VUEJS Course")
+
+
+// console.log("This is Js tutoiral to learn it");
+// console.log("This is Js tutoiral to learn it");
+// console.log("This is Js tutoiral to learn it");
+// console.log("This is Js tutoiral to learn it");
+// console.log("This is Js tutoiral to learn it");
+// console.log("This is Js tutoiral to learn it");
+// console.log("This is Js tutoiral to learn it");
+// console.log("This is Js tutoiral to learn it");
+
+
+// console.warn("Don't search these kind of words on YouTube");
+// console.warn("Don't search these kind of words on Google");
+// console.warn("Don't search these kind of words on Bing");
+// console.warn("Don't search these kind of words on Badu");
+// console.warn("Don't search these kind of words on Tiktok");
+// console.warn("Don't search these kind of words on Whatsapp");
+// console.warn("Don't search these kind of words on Instagram");
+// console.warn("Don't search these kind of words on Snapchat");
+
+
+// console.error("Payment failed.Please try again");
+// console.error("Transaction failed.Please try again");
+// console.error("Statment failed.Please try again");
+// console.error("Billing failed.Please try again");
+// console.error("Pin code failed.Please try again");
+// console.error("Password failed.Please try again");
+// console.error("Login failed.Please try again");
+// console.error("fetching data failed.Please try again");
+
+
+
+// document.write("<h1 style='font-size:20px; text-align:center;color:red;'>Registration Form</h1> <h2 style='font-size:20px; text-align:center;color:red;'>First Name: </h2>")
+
