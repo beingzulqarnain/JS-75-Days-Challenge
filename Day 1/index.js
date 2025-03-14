42462876e4cad8d100449049a0d8dd1cbe99ea1e
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2025-03-14 08:04:14 +0500

    Day - 1 Challenge Completed

---
+++ Day 1/index.js
@@ -0,0 +1,8 @@
+ // document.write("let's learn JavaScript from beginner to advanced level")
+        // alert("Hello JavaScript.....")
+        // console.log("Hello Js tutoirals");
+
+    //    console.log(123+1234*53-321);
+
+    console.log("This is Js tutorials");
+    
