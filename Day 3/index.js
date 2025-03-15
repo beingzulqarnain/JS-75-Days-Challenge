78699774a7f7f9edf9d533222236d80ba2914ae7
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2025-03-15 08:12:14 +0500

    Day - 3 Challenge Completed

---
+++ Day 3/index.js
@@ -0,0 +1,37 @@
+// Outpossibilities
+// ______________________________________________________
+
+// window.alert(123*4);
+// console.log();
+// document.write();
+
+// () --> Parenthesises 
+// = --> assignment opeartor
+
+// Data 
+// number -> 0-9
+// character --> a-z 
+// string --> "Hello friends"
+
+//keywords & Reserve Words 
+//-------------------------------------------------
+// var 
+// this 
+// for
+
+
+// Mathematical Approach 
+// 10 + 2 = 12
+
+// var x = 10;
+// var y = 2;
+// var z = x + y;
+// document.write(z)
+
+var a = 12;
+var b = 5;
+var c = 8;
+var d = 9;
+var e = 2 ;
+var f = (a*b)/c-d+e;
+document.write(f)
