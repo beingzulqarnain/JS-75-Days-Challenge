d7d88e5199429f6b7512e950ab8f66abc4c34c42
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2025-03-24 17:23:51 +0500

    Day - 4 Challenge Completed

---
+++ Day 4/script.js
@@ -0,0 +1,29 @@
+
+
+// we have three phase of programming language
+// 1. Low level language --> we build OS 
+// 2. Middle level language --> Mixex up of both
+// 3. High level language --> we build application
+
+
+
+// Low level language --> ada,fortran,kobol etc
+// Middle level language --> c++,Java,C etc
+// Hight level language --> Nodejs,JavaScript,python etc
+
+
+//Browser
+
+//Template Engine or Layout Engine
+// the name of template engine in google chrome is V8 Engine
+// In V8 Engine, we have two compiler
+// 1. Compiler  --> 20%
+// 2.Interpreter --> 80%
+// 3.Assembler
+
+
+//V8 Engine works to convert HTML,CSS and JavaScript into Browser to showcase to the client
+
+//Compiler execute once the code all in one
+// Interpreter execute line-by-line
+//Assembler execute to covert into binary number
---
+++ Day 5/script.js
@@ -0,0 +1,14 @@
+// Function
+// function demo(x) //parameters
+// {
+//   alert(x) 
+// }
+
+// demo("Right Anwser"); //arguments
+
+
+
+function sum (x=0,y=0){
+    var z = x + y;
+    alert(z)
+}
