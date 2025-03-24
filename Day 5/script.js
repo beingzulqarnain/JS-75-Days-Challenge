cfbc7f5f3e5b913553e99d9fa42a598af4b0c615
Zul Qarnain Afridi | MERN Stack Developer <beingzulqarnain@gmail.com>
2025-03-24 18:12:07 +0500

    Day - 5 Challenge Completed

--- Day 5/script.js
+++ Day 5/script.js
@@ -1,14 +1,14 @@
 // Function
-function demo(x) //parameters
-{
-  alert(x) 
-}
+// function demo(x) //parameters
+// {
+//   alert(x) 
+// }
 
 // demo("Right Anwser"); //arguments
 
 
 
-function sum (x=0,y=0){
-    var z = x + y;
-    alert(z)
-}
+// function sum (x=0,y=0){
+//     var z = x + y;
+//     alert(z)
+// }
